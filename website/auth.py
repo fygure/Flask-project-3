59662166a983f9760a7ef5ab7f375e7602d91cfb
fygure <92011471+fygure@users.noreply.github.com>
2022-10-25 11:49:54 -0500

    init templates

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,17 @@
-#Login page goes here
-# Blueprint - Allows for views to be declared here and defined in another place
+# Login, Logout, Signup routes live here.
+# Blueprint - Allows for views to be declared here and defined in another place.
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign-up</p>"
