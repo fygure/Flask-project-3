31e54f20348e4b576981b18343318c282487d12b
fygure <92011471+fygure@users.noreply.github.com>
2022-10-28 19:46:19 -0500

    added error handling for sign up form

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,40 @@
 # Login, Logout, Signup routes live here.
 # Blueprint - Allows for views to be declared here and defined in another place.
 from pydoc import render_doc
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logged Out</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords dont\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account Created.', category='success')
+            #add user to db
+
+        #print(email, firstName, password1)
+
+
     return render_template("sign_up.html")
